573942b594a2625313ff8d34a551a61a31547323
ericsos <ericlls@live.com>
2025-05-22 21:48:07 -0300

    dev: adicionado cadastro e login de operador e mais algumas tabelas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import customerRoute from './routes/customer_route.js';
 import categoryRoute from './routes/category_route.js';
 import checkoutRoute from './routes/checkout_route.js';
+import adminRoute from './routes/admin_route.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(json());
 app.use("/", customerRoute);
 app.use("/", categoryRoute);
 app.use("/", checkoutRoute);
+app.use("/", adminRoute);
 
 app.listen(port, () => {
     console.log(`App está executando na porta ${port}.`)
