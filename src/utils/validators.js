0b6ac0d7afe9137b401876a6a86dd28ffca25c45
ericsos <ericlls@live.com>
2025-06-03 17:51:13 -0300

    fix: checkout agora aceita cpf nulos (compra sem cpf na nota)

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,5 +1,9 @@
 export function formatarCPF(cpf) {
-  const numeros = cpf.replace(/\D/g, '');
-  if (numeros.length !== 11) return null;
-  return numeros.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  if (cpf) {
+    const numeros = cpf.replace(/\D/g, '');
+    if (numeros.length !== 11) return null;
+    return numeros.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  } else {
+    return null;
+  }
 }
