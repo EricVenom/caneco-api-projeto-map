b6db5da4432ecf54cd311d9a07c0b64e200e7f6f
codArtic0 <raulsousadesigner@gmail.com>
2026-03-04 19:05:14 -0300

    ADD: CORS

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import categoryRoute from './routes/category_route.js';
 import checkoutRoute from './routes/checkout_route.js';
 import adminRoute from './routes/admin_route.js';
 import { swaggerUi, swaggerSpec } from "./config/swaggerConfig.js";
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express()
 const port = 3000
 const server = swaggerSpec.servers?.[0].url
 
-app.use(json());
+app.use(cors())
+app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/", customerRoute);
